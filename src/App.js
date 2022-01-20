6ae6d27f566ab4f20f39442a2b4472355a891200
izuru775 <izuru775@gmail.com>
2022-01-21 03:45:56 +1100

    About page modified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackForm from './components/FeedbackForm';
 import { v4 as uuidv4 } from 'uuid';
 import AboutPage from './pages/AboutPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -42,6 +43,7 @@ function App() {
           ></Route>
           <Route path="/about" element={<AboutPage />} />
         </Routes>
+        <AboutIconLink/>
       </div>
     </Router>
   );
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
 function AboutPage() {
-  return <div>About</div>;
+  return (<Card>
+      <h1>About this project</h1>
+      <p>React app for leave feedbacks for services received</p>
+      <Link to='/'>Back To Home</Link>
+  </Card>)
 }
 
 export default AboutPage;
