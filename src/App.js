19ea834fe3530fd587fefdbff990e978b650ced6
izuru775 <izuru775@gmail.com>
2022-01-21 02:56:29 +1100

    Feedback app completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import FeedbackData from "./data/FeedbackData";
-import FeedbackList from "./components/FeedbackList";
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4(); // To generate unique id
+    setFeedback([newFeedback, ...feedback]); // Since state is immutable, you need to create a new array with newfeedback
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback}/>
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,19 +1,23 @@
 //rfce
-import Card from './shared/Card'
-import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa';
+import Card from './shared/Card';
+import PropTypes from 'prop-types';
 
-function FeedbackItem({ item }) {
- 
+function FeedbackItem({ item,handleDelete }) {
 
+    
   return (
-    <Card >
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={()=> handleDelete(item.id)} className="close">
+          <FaTimes color='purple'/>
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
 
-FeedbackItem.prototype ={
-    item:PropTypes.object.isRequired
-}
+FeedbackItem.prototype = {
+  item: PropTypes.object.isRequired,
+};
 export default FeedbackItem;
