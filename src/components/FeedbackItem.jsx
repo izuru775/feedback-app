797df28bb97616eb25087d32a572f935f2b5c435
izuru775 <izuru775@gmail.com>
2022-01-20 21:36:44 +1100

    Feedbcak UI created

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,19 @@
+//rfce
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+
+function FeedbackItem({ item }) {
+ 
+
+  return (
+    <Card >
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
+}
+
+FeedbackItem.prototype ={
+    item:PropTypes.object.isRequired
+}
+export default FeedbackItem;
