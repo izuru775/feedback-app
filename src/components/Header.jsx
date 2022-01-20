95c01da4a9d08381f74537b7a5a598ff09e9ea5d
izuru775 <izuru775@gmail.com>
2022-01-21 03:58:25 +1100

    Home route added to title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,34 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function Header({ text, bgColor, textColor }) {
+function Header({ text, bgColor, textColor, textDecoration }) {
   const headerStyles = {
     backgroundColor: bgColor,
     color: textColor,
+    textDecoration
   };
   return (
     <header style={headerStyles}>
       <div className="container">
-        <h2>{text}</h2>
+        <Link to="/" style={headerStyles}>
+          <h2>{text}</h2>
+        </Link>
       </div>
     </header>
   );
 }
 
 Header.defaultProps = {
-  text: "Feedback App",
-  bgColor:'rgba(0,0,0,0.4)',
-  textColor:'#ff6a95'
+  text: 'Feedback App',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95',
+  textDecoration:'none'
 };
 
 Header.propTypes = {
   text: PropTypes.string,
-  bgColor:PropTypes.string,
-  textColor:PropTypes.string
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+  textDecoration: PropTypes.string,
 };
 export default Header;
